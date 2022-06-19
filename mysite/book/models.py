316820359afa3774154149269221874ec650b092
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-06-19 10:30:38 +0545

    nothing new

--- mysite/book/models.py
+++ mysite/book/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):#creating a table
+    author=models.CharField(max_length=50)#these are the columns
+    book_name=models.CharField(max_length=50)
+    isbn=models.IntegerField()
