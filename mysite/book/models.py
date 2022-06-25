ee7703e0f6d837c2a68578b02835cc9d9f93812c
AbhinashShrestha <mr.indestructible010@gmail.com>
2022-06-25 16:05:59 +0545

    Delete mysite directory

--- mysite/book/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Book(models.Model):#creating a table
-    author=models.CharField(max_length=50,null=True)#these are the columns
-    book_name=models.CharField(max_length=50,null=True)
-    isbn=models.IntegerField(null=True)
