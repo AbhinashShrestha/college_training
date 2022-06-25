d5ce473b3bcef6d57c54dd677e1cbd6fda9d2eda
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-06-25 15:47:33 +0545

    did something

--- mysite/book/models.py
+++ mysite/book/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):#creating a table
-    author=models.CharField(max_length=50)#these are the columns
-    book_name=models.CharField(max_length=50)
-    isbn=models.IntegerField()
+    author=models.CharField(max_length=50,null=True)#these are the columns
+    book_name=models.CharField(max_length=50,null=True)
+    isbn=models.IntegerField(null=True)
