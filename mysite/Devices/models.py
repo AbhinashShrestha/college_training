fab83ab7bc5fbfa7651710f3a327a54c10f03f28
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-06-25 16:34:46 +0545

    added new app

---
+++ mysite/Devices/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Laptop(models.Model):
+    manufacture=models.CharField(max_length=50,null=True,blank=True)
+    name=models.CharField(max_length=20,null=True,blank=True)
+    ram=models.CharField(max_length=20,null=True,blank=True)
+    gpu=models.CharField(max_length=20,null=True,blank=True)
+    cpu=models.CharField(max_length=20,null=True,blank=True)
+    price=models.DecimalField(max_digits=50,decimal_places=2,null=True,blank=True)
+def _str_(self):
+    return(self.name)
