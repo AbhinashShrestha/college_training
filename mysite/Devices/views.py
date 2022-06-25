e435a05931ba8fbc36c4e66031c24aa2b9233474
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-06-25 17:32:26 +0545

    made a new page

--- mysite/Devices/views.py
+++ mysite/Devices/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from Devices.models import Laptop
+
 # Create your views here.
+def index(request):
+    laptop_list=Laptop.objects.all()
+    laptops={
+        'laptops':laptop_list
+    }
+    return render(request,'Devices/laptop-list.html',laptops)#'app_name/file.html'
