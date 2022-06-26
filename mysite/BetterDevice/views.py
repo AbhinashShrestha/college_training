89197bf0035dcb48a9cd188a9ef7b9cf80b2f03a
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-06-26 11:06:34 +0545

    made some

--- mysite/BetterDevice/views.py
+++ mysite/BetterDevice/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView,TemplateView,DeleteView,UpdateView,DetailView
+
+from BetterDevice.models import Laptop
 # Create your views here.
 
+class LaptopListView(ListView):
+    model=Laptop
+    BetterDevice='BetterDevice/laptop-list.httml'
+    context_object_name='laptop'
