aed60c3f736f7fc490ae79a3cfe6d447708a0fa6
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-07-02 16:38:44 +0545

    done

--- mysite/BetterDevice/views.py
+++ mysite/BetterDevice/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator
 from BetterDevice.models import Laptop
 # Create your views here.
 from .forms import LaptopRegistration
+
 class LaptopListView(ListView):
     model=Laptop
     template_name='BetterDevice/laptop-list.html'
@@ -12,12 +13,12 @@ class LaptopListView(ListView):
     
     def get_context_data(self,*args ,**kwargs):
         laptops=self.get_queryset()#laptop is name of model
-        paginator=Paginator(laptops,5)
+        paginator=Paginator(laptops,7)
         page_number=self.request.GET.get('page')
         page_obj=paginator.get_page(page_number)
         context={'laptops':page_obj}
         return context
 class LaptopCreateView(CreateView):
     form_class=LaptopRegistration
-    template_name='BetterDevices/laptop-create.html'
+    template_name='BetterDevice/laptop-create.html'
     success_url=reverse_lazy('laptop-list')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-# from Devices import views
+# from BetterDevice import views
 
 
 urlpatterns = [
