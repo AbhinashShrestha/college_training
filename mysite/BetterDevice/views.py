191b505f4c1cd9634daf3ee4004d3af78b285c9d
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-07-02 15:53:57 +0545

    ok

--- mysite/BetterDevice/views.py
+++ mysite/BetterDevice/views.py
@@ -1,7 +1,6 @@
-# from re import template
 from django.shortcuts import render
 from django.views.generic import ListView,TemplateView,DeleteView,UpdateView,DetailView
-
+from django.core.paginator import Paginator
 from BetterDevice.models import Laptop
 # Create your views here.
 
@@ -9,3 +8,11 @@ class LaptopListView(ListView):
     model=Laptop
     template_name='BetterDevice/laptop-list.html'
     context_object_name='laptops'
+    
+    def get_context_data(self,*args ,**kwargs):
+        laptop=self.get_queryset()#laptop is name of model
+        paginator=Paginator(laptop,5)
+        page_number=self.request.GET.get('page')
+        page_obj=paginator.get_page(page_number)
+        context={'laptop':page_obj}
+        return context
--- mysite/Devices/urls.py
+++ mysite/Devices/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('delete/<int:id>',views.delete,name='delete'),
     path('create',views.create,name='create'),
     path('update/<int:id>',views.update,name='update'),
-
 ]
