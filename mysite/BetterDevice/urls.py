401e561b546effce103ff6db4b42f3ae85c63be1
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-07-02 17:22:15 +0545

    ok

--- mysite/BetterDevice/urls.py
+++ mysite/BetterDevice/urls.py
@@ -6,4 +6,5 @@ from BetterDevice import views
 urlpatterns = [
     path('',views.LaptopListView.as_view(),name='laptop-list'),
     path('create',views.LaptopCreateView.as_view(),name='laptop-create'),
+    path('<int:pk>/update',views.LaptopUpdateView.as_view(),name='laptop-update'),
 ]
