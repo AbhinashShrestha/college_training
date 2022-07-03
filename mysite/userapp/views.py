53e7f183ea8d7c0416f8f11106b787361f922849
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-07-03 09:57:57 +0545

    ok

--- mysite/userapp/views.py
+++ mysite/userapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate,login,logout
 from userapp.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm,PasswordResetForm
 # Create your views here.
 def register(request):
     if request.method =="POST":
@@ -10,7 +11,6 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-
             raw_password=form.cleaned_data.get('password1')
             user=authenticate(username=username,password=raw_password)
             # login(request,user)
@@ -20,3 +20,20 @@ def register(request):
         form=RegisterForm()
     context={"form":form}
     return render(request,'userapp/register.html',context)
+def user_login(request):
+    if request.method=='POST':
+        form=AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            raw_password=form.cleaned_data.get('password1')
+            user=authenticate(username=username,password=raw_password)
+            if user is not None:
+                login(request,user)
+                return redirect('laptop-list')
+            else:
+                messages.error(request,"You are not registered yet")
+        else:
+            messages.error(request,"Invalid credentials")
+    form = AuthenticationForm()
+    return render(request,'userapp/login.html',{'form':form})
