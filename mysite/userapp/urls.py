6aef0754fb1a01488c39a286e25edbbfa8c29013
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-07-03 10:26:43 +0545

    ok

--- mysite/userapp/urls.py
+++ mysite/userapp/urls.py
@@ -1,7 +1,7 @@
-from django.urls import include, path
+from django.urls import path
 from userapp import views
 
 urlpatterns = [
     path('register/',views.register,name="register"),
-    path('login/',views.user_login,name="user_login"),
+    path('login/',views.user_login,name="login"),
 ]
