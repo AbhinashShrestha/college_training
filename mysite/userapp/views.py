fa8f535383a5e7472aa05b8b8b0c266fc6897a8e
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-07-03 08:50:04 +0545

    ok

--- mysite/userapp/views.py
+++ mysite/userapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate,login,lagout
+from userapp.forms import RegisterForm
 
 # Create your views here.
+def register(request):
+    if request.method =="POST":
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            raw_password=form.cleaned_data.get('password1')
+            user=authenticate(username=username,password=raw_password)
+            # login(request,user)
+            messages.success(request,"Registration sucessful")
+            return redirect('user_login')
+    else:
+        form=RegisterForm()
+    context={"form":form}
+    return render(request,'usermanagement/register.html',context)
