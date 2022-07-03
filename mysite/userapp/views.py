ba4e85c36358b737e98b6b08b491d906f3f084d9
Abhinash Shrestha <mr.indestructible010@gmail.com>
2022-07-03 09:38:53 +0545

    ok

--- mysite/userapp/views.py
+++ mysite/userapp/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate,login,logout
 from userapp.forms import RegisterForm
 from django.contrib import messages
+import ipdb;ipdb.set_trace()
 # Create your views here.
 def register(request):
     if request.method =="POST":
         form=RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            import ipdb;ipdb.set_trace()
             username=form.cleaned_data.get('username')
 
             raw_password=form.cleaned_data.get('password1')
